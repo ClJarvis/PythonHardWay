8339be43584869f4a9f8c09187a85f4b31280703
Chris Jarvis <clj@ollure.com>
2016-02-09 22:22:29 -0600

    python: numbers and math

---
+++ ex3.py
@@ -0,0 +1,25 @@
+#this will print the line
+print "I will now count my chickens:"
+#This will print Hens and the reult of the calculation
+print "Hens", 25 + 30 / 6
+#This will print roosters and the result of the equation
+print "Roosters", 100 - 25 * 3 % 4
+#print line
+print "Now I will count the eggs:"
+#print result of math
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#print string no math cause equation is in a string
+print "Is it true that 3 + 2 < 5 - 7?"
+#print false because this statement says 5 is less tha -2, 5 is greater than negative 2 so the statement is false
+print 3 + 2 < 5 -7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+#the remaining lines will print the string and true or false depending on result of calculation.
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
