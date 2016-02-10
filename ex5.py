e01c893c6cad512715c1fbb5fa2b2fbe8e951959
Chris Jarvis <clj@ollure.com>
2016-02-09 23:31:24 -0600

    variables and printing

---
+++ ex5.py
@@ -0,0 +1,16 @@
+my_name = "Zed Leppin aka Zeddicus Zu'l Zorander"
+my_age = 35 #more or less
+my_height = 74 # inches
+my_weight = 180 #snark
+my_eyes = 'Blue'
+my_teeth = "white"
+my_hair = 'Brown'
+
+print "Lets talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "wow filler line"
+print "He's got %s eyes and %s hair" % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
