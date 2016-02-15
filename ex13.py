313750333ebc72491f77b9748b0459972f9d1d57
Chris Jarvis <clj@ollure.com>
2016-02-15 00:06:00 -0600

    fixed typo, reran with parameter on command line works now

--- ex13.py
+++ ex13.py
@@ -1,8 +1,8 @@
 from sys import argv
 
-script. frist, second, third = argv
+script, first, second, third = argv
 
 print "The script is called:", script
-print "your frist variable is:", frist
+print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
