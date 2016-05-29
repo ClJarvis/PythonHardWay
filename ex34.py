7076113c0399d52f8b1df63e6373c2100369e724
Chris Jarvis <clj@ollure.com>
2016-05-29 01:01:22 -0500

    ex34: lists

---
+++ ex34.py
@@ -0,0 +1,20 @@
+''' 1. the animal at 1 is python is the second anmial.  the second animal is at 1 and is a python.
+2. the third animal is at 2 and is a peacock. the animal at 2 is the third animal and is a peacock.
+3. the first animal is bear is at 0, the animal at 0 is the frist animal and is a bear.
+4. the animal at 3 is kangaroo is the forth animal. the forth animal is at 3 and is a kangaroo
+5. the fifth animal is at 4 and is a whale. the animal at 4 is the fifth animal and is a whale
+6. the animal at 2 is peacock, the animal at 2 is the third animal and is a peacock.
+7. The sixth animal is at 5 and is a platypus. the animala at 5 is the sixth animal and is a platypus
+8. The animal at 4 is the fifth animal and is a whale. the fifth animal is at 4 and is a whale.
+'''
+
+#verifying answers in python
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+print animals[1]
+print animals[2]
+print animals[0]
+print animals[3]
+print animals[4]
+print animals[2]
+print animals[5]
+print animals[4]
